836892f789ab34e1b5b53a7f6ec292737288c3c1
Dnet <dnet@dissenter.net>
2026-01-08 19:03:08 -0500

    1

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Comprehensive status bar configuration for dwmblocks-dnet
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g", 5, 0},
+	{"", "pacman -Qu | wc -l", 3000, 0},
+	{"", "date '+%I:%M %p'", 5, 0},
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+// sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 3;
